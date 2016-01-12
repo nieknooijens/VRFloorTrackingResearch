4e7301a0eb568e14cc00989216b9461bb606b381
Nooijens,niek N.P.E <n.nooijens@student.fontys.nl>
2016-01-12 14:33:26 +0100

    final prototypes

--- RockVR/Assets/Cardboard/DemoScene/playercontroller.cs
+++ RockVR/Assets/Cardboard/DemoScene/playercontroller.cs
@@ -5,6 +5,7 @@ public class playercontroller : MonoBehaviour {
 	
 	public string url; //FIXME hardcoded URL
 	public string coordinates = "{0,0}";
+	private string previouscoordinates = "{0,0}";
 	public WWW mycoord;
 	private bool previouslyDone = true;
 
@@ -22,24 +23,31 @@ public class playercontroller : MonoBehaviour {
 			Debug.Log(coordinates);
 			previouslyDone = true;
 		}
-		string x = "";
-		string y = "";
-		int index = 1;
-		char nextchar = '0';
-		while (nextchar != ',') {
-			x += coordinates[index];
+		if (previouscoordinates != coordinates) {
+			string x = "";
+			string y = "";
+			int index = 1;
+			char nextchar = '0';
+			while (nextchar != ',') {
+				x += coordinates [index];
+				index++;
+				nextchar = coordinates [index];
+			}
 			index++;
-			nextchar = coordinates[index];
+			while (nextchar != '}') {
+				y += coordinates [index];
+				index++;
+				nextchar = coordinates [index];
+			}
+			float newX = 0 + (float.Parse (x) * 2);
+			float newZ = 0 - (float.Parse (y) * 2);
+			transform.position = new Vector3 (newX, 1.5f, newZ);
+		} else {
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+			Vector3 movement = new Vector3 (moveHorizontal / 10, 0.0f, moveVertical / 10);
+			transform.position = transform.position + movement;
 		}
-		index++;
-		while (nextchar != '}') {
-			y += coordinates[index];
-			index++;
-			nextchar = coordinates[index];
-		}
-		float newX = 0 + (float.Parse(x) * 2);
-		float newZ = 0 - (float.Parse(y) * 2);
-		transform.position = new Vector3(newX, 1.5f,newZ);
 		
 	}
 }
